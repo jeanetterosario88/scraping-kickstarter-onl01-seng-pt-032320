32d1a969f22fc57304f60e2249e12c157b14d41e
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-05-04 03:24:47 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,7 +10,7 @@
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     #
      
-    def create_project_hash
+  def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
  
